fa9ebb1e1d21fe84fea46cfc1dc741db964d06b7
IZAEL DE SOUZA COSTA <izael.costa@escola.pr.gov.br>
2026-06-17 15:30:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function respostas(status) {
+    if (status === 'Izael') {
+        alert('Parabéns, você acertou!');
+    } else {
+        alert('Resposta incorreta, tente novamente!');
+    }
+
+
+ 
+}
