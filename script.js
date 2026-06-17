e12cf876e737a00f5c4a904a26c854f7b261bef7
IZAEL DE SOUZA COSTA <izael.costa@escola.pr.gov.br>
2026-06-17 15:55:42 -0300

    Enhance alert message with additional information

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function respostas(status) {
     if (status === 'Izael') {
-        alert('Parabéns, você acertou!');
+        alert('Parabéns, você acertou! Eu nasci em Santo Antônio da Platina');
     } else {
         alert('Resposta incorreta, tente novamente!');
     }
